7be81073cde55e1bffd0ac9a49a529fe7b02b47d
lafierro4 <lafierro4@miners.utep.edu>
2022-02-01 09:12:16 -0700

    Modified Files

--- draw_shapes.c
+++ draw_shapes.c
@@ -25,4 +25,9 @@ void print_triangle(int leftCol, int size)
     putchar('\n');
   }
 }
+//print arrow
+void print_arrow(int leftCol, int size){
+  print_triangle(leftCol,size);
+  print_square(leftCol+ size/2 +1, size);
+}
 
